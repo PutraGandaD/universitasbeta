5cf63ad145e137eafcc9bcc7c2d310543f45db65
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2022-12-14 14:09:00 +0700

    Added new changes (1)

---
+++ app/src/main/java/com/if3b/universitasplg/ModelUniversitas.java
@@ -0,0 +1,49 @@
+package com.if3b.UniversitasPLG;
+
+public class ModelUniversitas {
+    private String nama_univ;
+    private String tentang_univ;
+    private String foto_univ;
+    private String logo_univ;
+    private String alamat_univ;
+
+    public String getNama_univ() {
+        return nama_univ;
+    }
+
+    public void setNama_univ(String nama_univ) {
+        this.nama_univ = nama_univ;
+    }
+
+    public String getTentang_univ() {
+        return tentang_univ;
+    }
+
+    public void setTentang_univ(String tentang_univ) {
+        this.tentang_univ = tentang_univ;
+    }
+
+    public String getFoto_univ() {
+        return foto_univ;
+    }
+
+    public void setFoto_univ(String foto_univ) {
+        this.foto_univ = foto_univ;
+    }
+
+    public String getLogo_univ() {
+        return logo_univ;
+    }
+
+    public void setLogo_univ(String logo_univ) {
+        this.logo_univ = logo_univ;
+    }
+
+    public String getAlamat_univ() {
+        return alamat_univ;
+    }
+
+    public void setAlamat_univ(String alamat_univ) {
+        this.alamat_univ = alamat_univ;
+    }
+}
